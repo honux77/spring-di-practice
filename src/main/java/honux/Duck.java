1fa14c138c4a7099b40dd35c367dbbbff4735875
Hoyoung Jung <honux77@gmail.com>
2023-03-24 08:24:46 +0900

    생성자로 의존성 주입

--- src/main/java/honux/Duck.java
+++ src/main/java/honux/Duck.java
@@ -4,13 +4,7 @@ public class Duck {
 
     private Wing wing;
 
-    public Duck(){}
-
     public Duck(Wing wing) {
-        this.wing = wing;
-    }
-
-    public void setWing(Wing wing) {
         this.wing = wing;
     }
 
